4276b5b27ddae551c80e3f85321362f3a86351c0
Priyang1310 <consistentcoderpriyangdesai@gmail.com>
2024-06-11 11:19:13 +0530

    commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,68 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc/SectionWrapper";
-import { technologies } from "../constants";
+// import { technologies } from "../constants";
+import {
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  threejs,
+} from "../assets";
 
 const Tech = () => {
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+  ];
+
   return (
     <div className="flex flex-wrap justify-center gap-10">
       {technologies.map((technology, index) => (
@@ -14,4 +74,4 @@ const Tech = () => {
   );
 };
 
-export default Tech
+export default Tech;
